b7d73034f8f06598e08881066c3c1925762ed2cf
Patrick Manion <patrickjmanion@gmail.com>
2021-04-05 23:21:01 -0500

    Add live Alchemy API

--- dai-eth-monitor.mjs
+++ dai-eth-monitor.mjs
@@ -1,14 +1,23 @@
 import { ChainId, WETH, Fraction, Pair, Token, TokenAmount, Fetcher } from '@uniswap/sdk';
+import { ethers } from 'ethers';
+
+const alchemyApiKey = process.env.ALCHEMY_ETHEREUM_DATA_KEY;
+
+const alchemy = new ethers.providers.AlchemyProvider(ChainId.MAINNET, alchemyApiKey);
 
 const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18);
 
-let timer = 1;
+let maxIterations = 100;
+let intervalSeconds = 10;
+
+let counter = 0;
 
 let interval = setInterval(async () => {
-  timer++;
+  counter++;
 
-  const pair = await Fetcher.fetchPairData(DAI, WETH[DAI.chainId]);
+  const pair = await Fetcher.fetchPairData(DAI, WETH[DAI.chainId], alchemy);
   console.log(`DAI per ETH: ${pair.token1Price.toSignificant(7)}`);
 
-  if (timer > 2) clearInterval(interval);
-}, 5000);
+  if (counter > maxIterations) clearInterval(interval);
+
+}, 1000 * intervalSeconds);
